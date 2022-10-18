83ba223514029f057adc46c6a231ddbb73d8b67e
Dexveloper <59431285+Dexveloper@users.noreply.github.com>
2022-10-17 17:13:10 -0700

    Create ProtocolInfo

---
+++ src/shoghicp/BigBrother/network/ProtocolInfo.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * @author BlockNetworks
+ * @link https://github.com/BlockNetworks/BigBrother
+ */
+
+declare(strict_types=1);
+
+namespace shoghicp\BigBrother\network;
+
+class ProtocolInfo {
+    const ALLOWED_PROTOCOLS = [422, 428, 431, 440, 448, 465, 471, 475, 486, 503];
+}
